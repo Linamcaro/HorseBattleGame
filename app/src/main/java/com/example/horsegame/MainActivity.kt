ef71fe2fab44e62dc6d7d0bd8a22f4d6d2149a5b
Lina <41321441+Linamcaro@users.noreply.github.com>
2023-09-09 15:40:13 -0500

    add random inital position

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -10,13 +10,44 @@ import android.widget.LinearLayout
 import android.widget.TableRow
 
 class MainActivity : AppCompatActivity() {
+
+    private var cellSelected_x = 0
+    private var cellSelected_y = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         startScreenGame()
+        initialPosition()
 
     }
+
+    //Set the new position randomly
+    private fun initialPosition(){
+        var x = 0
+        var y = 0
+
+        x= (0..7).random()
+        y =(0..7).random()
+
+        cellSelected_x = x
+        cellSelected_y = y
+        selectCell(x,y)
+    }
+
+    private fun selectCell(x: Int, y: Int) {
+        paintHorseCell(cellSelected_x, cellSelected_y, "previous_cell")
+
+        cellSelected_x = x
+        cellSelected_y = y
+
+        paintHorseCell(cellSelected_x, cellSelected_y, "selected_cell")
+    }
+
+    private fun paintHorseCell(cellselectedX: Int, cellselectedY: Int, s: String) {
+        var cell:ImageView = findViewById(resources.getIdentifier("c$column$row", "id", packageName))
+    }
+
 
     private fun startScreenGame() {
         setBoardSize()
