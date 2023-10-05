3663c6c6b5877965049a4fb57fd18828109517ae
Lina <41321441+Linamcaro@users.noreply.github.com>
2023-10-04 21:31:03 -0500

    show available movement options

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TableRow
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,11 @@ class MainActivity : AppCompatActivity() {
 
     private var cellSelected_x = 0
     private var cellSelected_y = 0
+
+    private var options = 0
+
+    private var nameColorBlack = "black_cell"
+    private var nameColorWhite = "white_cell"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,6 +43,14 @@ class MainActivity : AppCompatActivity() {
 
     //check if the cell that the player selected can ba painted.
     private fun checkCell(x: Int, y: Int) {
+
+        /*
+        0 cell is free
+        1 cell is marked
+        2 is a bonus
+        9 option for the current movement
+     */
+
         var dif_x = x - cellSelected_x
         var dif_y = y - cellSelected_y
 
@@ -89,6 +103,7 @@ class MainActivity : AppCompatActivity() {
         cellSelected_y = y
 
         selectCell(x,y)
+
     }
 
     private fun selectCell(x: Int, y: Int) {
@@ -102,7 +117,97 @@ class MainActivity : AppCompatActivity() {
         cellSelected_x = x
         cellSelected_y = y
 
+        clearOptions()
+
         paintHorseCell(cellSelected_x, cellSelected_y, "selected_cell")
+
+        checkOption(x,y)
+    }
+
+    private fun clearCellOptine(x: Int, y: Int) {
+
+        var cell:ImageView = findViewById(resources.getIdentifier("c$x$y", "id", packageName))
+
+        if(checkColorCell(x,y) == "black") {
+            cell.setBackgroundResource(
+                ContextCompat.getColor(
+                    this, resources.getIdentifier(nameColorBlack, "color", packageName)
+                )
+            )
+        }
+            else cell.setBackgroundResource(ContextCompat.getColor(
+            this,resources.getIdentifier(nameColorWhite,"color",packageName)))
+    }
+
+    private fun clearOptions() {
+        for(i in 0..7){
+            for(j in 0..7){
+                if(board[i][j] == 9 || board[i][j] == 2) {
+                    if(board[i][j] == 9) {
+                        board[i][j] = 0
+                        clearCellOptine(i,j)
+                    }
+
+                }
+            }
+        }
+    }
+
+
+
+    //check all the movement options
+    private fun checkOption(x: Int, y: Int){
+        options = 0
+
+        checkMove(x, y ,1 ,2) //right - top long
+        checkMove(x, y, 1 ,-2)  //right - bottom long
+        checkMove(x, y, 2, 1) //right long- top
+        checkMove(x, y, 2, -1) //right long - bottom
+        checkMove(x, y, -1, 2) //left - top long
+        checkMove(x, y, -1, -2)  //left - bottom long
+        checkMove(x, y, -2, 1)  //left long - top
+        checkMove(x ,y, -2, -1)  //left long - bottom
+
+        var optionsData = findViewById<TextView>(R.id.optionsData)
+        optionsData.text = options.toString()
+
+    }
+
+    private fun checkMove(x: Int, y: Int, movX: Int, movY: Int) {
+
+        var option_x = x + movX
+        var option_y = y + movY
+
+        if(option_x in 0..7 && option_y in 0..7) {
+
+            if(board[option_x][option_y] == 0 || board[option_x][option_y] == 2) {
+                options++
+
+                paintOptions(option_x, option_y)
+
+                board[option_x][option_y]
+            }
+        }
+
+    }
+
+    private fun paintOptions(optionX: Int, optionY: Int) {
+        var cell:ImageView = findViewById(resources.getIdentifier("c$optionX$optionY", "id", packageName))
+        if(checkColorCell(optionX,optionY) == "black") cell.setBackgroundResource(R.drawable.option_black)
+        else cell.setBackgroundResource(R.drawable.option_white)
+        
+    }
+
+    private fun checkColorCell(x: Int, y: Int): String {
+        var color = ""
+        //cells where the black cells are located
+        var blackColumn_x = arrayOf(0,2,4,6)
+        var blackrow_x = arrayOf(1,3,5,7)
+
+        if(blackColumn_x.contains(x) && blackColumn_x.contains(y) || blackrow_x.contains(x) && blackrow_x.contains(y)) color = "black"
+        else color = "white"
+
+        return color
     }
 
     //Paint the horse on the selected cell
